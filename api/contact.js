f6a093a037dea1c740b7086cd11632980ddf9343
Alejandro Moreno <alexmoreno@scndal.com>
2026-07-30 01:00:21 -0600

    feat: agregar andrea.r y michel.l a destinatarios de cotización

--- api/contact.js
+++ api/contact.js
@@ -39,7 +39,7 @@ export default async function handler(req, res) {
       },
       body: JSON.stringify({
         from: 'Steel Paint <contacto@mail.steel-paint.com.mx>',
-        to: ['hola@scndal.com', 'marcelo.steelpaint@gmail.com', 'marcelo@steel-paint.com.mx'],
+        to: ['hola@scndal.com', 'marcelo.steelpaint@gmail.com', 'marcelo@steel-paint.com.mx', 'andrea.r@scndal.com', 'michel.l@scndal.com'],
         subject: hasCalc
           ? `Actualización de lead: ${nombre} / ${empresa}`
           : mensaje
